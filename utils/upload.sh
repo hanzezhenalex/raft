8b09c8bfa09a4bc96ac121ba51c82e833568d5fd
I548435 <alex.han@sap.com>
2023-04-21 10:16:18 +0800

    use passwd instead

--- utils/upload.sh
+++ utils/upload.sh
@@ -22,11 +22,7 @@ if [ -d "${LOG_PATH}" ];then
 
   transferToHTML $LOG_PATH
 
-  PEM_FILE="key.pem"
-  echo "${SSH_KEY}" | tr -d '\r' > $PEM_FILE
-  chmod 400 $PEM_FILE
-
-  scp -i "${PEM_FILE}" -o StrictHostKeyChecking=no -r $LOG_PATH "${USERNAME}"@"${SERVER}":/tmp/push-val/"$HEAD_REF"."$SHA"
+  sshpass -p "${SSH_PASSWD}" scp -r $LOG_PATH "${USERNAME}"@"${SERVER}":/tmp/push-val/"$HEAD_REF"."$SHA"
 
   exit 1
 fi
