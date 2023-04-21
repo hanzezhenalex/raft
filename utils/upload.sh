6d2a3d0e5a1a3791ccd295ea6077b2f94ec66aae
I548435 <alex.han@sap.com>
2023-04-21 10:52:45 +0800

    correct username

--- utils/upload.sh
+++ utils/upload.sh
@@ -12,7 +12,6 @@ transferToHTML() {
             transferToHTML $file
         else
             python ./utils/logExplorer.py --log $file --out $file.html
-            rm $file
         fi
     done
 }
@@ -22,7 +21,7 @@ LOG_PATH="./logs"
 if [ -d "${LOG_PATH}" ];then
   echo "start uploading logs..."
   transferToHTML $LOG_PATH
-  sshpass -p "${SSH_PASSWD}" scp -r $LOG_PATH "${USERNAME}"@"${SERVER}":/tmp/push-val/"$HEAD_REF"."$SHA"
+  sshpass -p "${SSH_PASSWD}" scp -r $LOG_PATH "${USERNAME}"@"${SERVER}":~/tmp/push-val/"$SHA"
   exit 1
 fi
 
